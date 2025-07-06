928e8df3261a7940ab7afa67cf5433cf7b8cd9c5
LankyMoose <rausten93@gmail.com>
2025-07-06 22:10:03 +1200

    fox router tutorial missing navigation

--- src/pages/tutorial/5-routing/files/App.jsx
+++ src/pages/tutorial/5-routing/files/App.jsx
@@ -1,9 +1,14 @@
-import { Router, Route } from "kaioken/router"
+import { Router, Route, Link } from "kaioken/router"
 
 export function App() {
   return (
     <div style={{ fontFamily: "sans-serif", padding: "20px" }}>
       <h1>My App</h1>
+
+      <nav style={{ display: "flex", gap: "10px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
 
       <Router>
         <Route path="/" element={<div>Welcome to the home page!</div>} />
