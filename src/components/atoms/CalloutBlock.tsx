4a1157b714529e0d7569224b5e71542d3c89b8a0
LankyMoose <rausten93@gmail.com>
2026-03-24 23:54:12 +1300

    update docs for kiru@1.4

--- src/components/atoms/CalloutBlock.tsx
+++ src/components/atoms/CalloutBlock.tsx
@@ -1,13 +1,25 @@
+import { className as cls } from "kiru/utils"
+interface CalloutBlockVariantProps {
+  fontSize?: FontSize
+  prose?: boolean
+  className?: string
+  children: JSX.Children
+}
+
 function DefaultCalloutBlock({
   fontSize = "small",
+  prose,
   children,
-}: {
-  fontSize?: FontSize
-  children: JSX.Children
-}) {
+  className,
+}: CalloutBlockVariantProps) {
   return (
     <div
-      className={`${fontSize === "small" ? "text-sm" : ""} callout-block not-prose px-4 py-2 bg-dark/50 backdrop-blur-md text-neutral-200 rounded-sm border-l-4 border-l-[#b42641]`}
+      className={cls(
+        fontSize === "small" && "text-sm",
+        prose ? "prose prose-invert" : "not-prose",
+        `callout-block px-2 py-2 bg-dark/50 backdrop-blur-md text-neutral-200 rounded-sm border-l-4 border-l-[#b42641]`,
+        className
+      )}
     >
       {children}
     </div>
@@ -16,14 +28,18 @@ function DefaultCalloutBlock({
 
 function InfoCalloutBlock({
   fontSize = "small",
+  prose,
   children,
-}: {
-  fontSize?: FontSize
-  children: JSX.Children
-}) {
+  className,
+}: CalloutBlockVariantProps) {
   return (
     <div
-      className={`${fontSize === "small" ? "text-sm" : ""} callout-block not-prose px-4 py-2 bg-info backdrop-blur-md text-neutral-200 rounded-sm`}
+      className={cls(
+        fontSize === "small" && "text-sm",
+        prose ? "prose prose-invert" : "not-prose",
+        `callout-block px-2 py-2 bg-white/7.5 backdrop-blur-md text-neutral-200 rounded-sm`,
+        className
+      )}
     >
       {children}
     </div>
@@ -32,14 +48,18 @@ function InfoCalloutBlock({
 
 function WarningCalloutBlock({
   fontSize = "small",
+  prose,
   children,
-}: {
-  fontSize?: FontSize
-  children: JSX.Children
-}) {
+  className,
+}: CalloutBlockVariantProps) {
   return (
     <div
-      className={`${fontSize === "small" ? "text-sm" : ""} callout-block not-prose px-4 py-2 border-2 border-dashed border-amber-400/10 bg-amber-400/8 backdrop-blur-md text-neutral-200 rounded-sm`}
+      className={cls(
+        fontSize === "small" && "text-sm",
+        prose ? "prose prose-invert" : "not-prose",
+        `callout-block px-2 py-2 border-2 border-dashed border-amber-400/10 bg-amber-400/8 backdrop-blur-md text-neutral-200 rounded-sm`,
+        className
+      )}
     >
       {children}
     </div>
@@ -47,31 +67,17 @@ function WarningCalloutBlock({
 }
 
 type FontSize = "small" | "normal"
-type CalloutBlockProps = {
-  children: JSX.Children
+interface CalloutBlockProps extends CalloutBlockVariantProps {
   variant?: "default" | "info" | "warning"
-  fontSize?: FontSize
 }
 
-export function CalloutBlock({
-  children,
-  variant,
-  fontSize,
-}: CalloutBlockProps) {
+export function CalloutBlock({ variant, ...props }: CalloutBlockProps) {
   switch (variant) {
     case "info":
-      return <InfoCalloutBlock fontSize={fontSize}>{children}</InfoCalloutBlock>
+      return <InfoCalloutBlock {...props} />
     case "warning":
-      return (
-        <WarningCalloutBlock fontSize={fontSize}>
-          {children}
-        </WarningCalloutBlock>
-      )
+      return <WarningCalloutBlock {...props} />
     default:
-      return (
-        <DefaultCalloutBlock fontSize={fontSize}>
-          {children}
-        </DefaultCalloutBlock>
-      )
+      return <DefaultCalloutBlock {...props} />
   }
 }
